98564f50f092276e0f0e860bf65e286de196f78d
sonicheo <1sonicheo@gmail.com>
2022-11-18 21:51:49 -0800

    Finished questions 1 and 2 in python

--- trainheroic-code-test/code/Python/answer.py
+++ trainheroic-code-test/code/Python/answer.py
@@ -1,20 +1,6 @@
 import json
 
-# Collect answers
-answers = {
-    # In total, how many pounds have these athletes Bench Pressed?
-    'q1': None,
 
-    # How many pounds did Barry Moore Back Squat in 2016?
-    'q2': None,
-
-    # In what month of 2017 did Barry Moore Back Squat the most total weight?
-    'q3': None,
-
-    # What is Abby Smith's Bench Press PR weight?
-    # PR defined as the most the person has ever lifted for that exercise, regardless of reps performed.
-    'q4': None
-}
 
 
 # Load data
@@ -35,4 +21,74 @@ json_file.close()
 
 
 # Output answers
-print json.dumps(answers)
+
+# HelperFunctions
+# Finds the userId of the user whose lastName and firstName match
+def findUserID(firsName, lastName):
+    for user in users:
+        if user['name_first'] == firsName and user['name_last'] == lastName:
+            return(user['id'])
+
+def findExerciseID(exerciseName):
+    for exercise in exercises:
+        if exercise['title'] == exerciseName:
+            return exercise['id']
+
+def totalWeightInBlock(blocks):
+    weight = 0
+    for block in blocks:
+        for set in block['sets']:
+            if set['weight']:
+                weight += int(set['weight'])
+    return weight
+
+  
+#question funtions 
+def q1():
+    pounds = 0
+    for workout in workouts:
+        pounds += totalWeightInBlock(workout['blocks'])
+    return pounds        
+
+def q2():
+    userID = findUserID('Barry', 'Moore')
+    pounds = 0  
+    for workout in workouts:
+        if workout['user_id'] == userID and '2016' in workout['datetime_completed']:
+            pounds += totalWeightInBlock(workout['blocks'])
+    
+    return pounds
+
+def q3():
+    exerciseId = findExerciseID("Back Squat")
+    userID = findUserID('Barry', 'Moore')
+    
+    for workout in workouts:
+        if workout['user_id'] == userID and '2017' in workout['datetime_completed']:
+            print(workout)
+    
+
+def q4():
+    pass
+
+q3()
+q4()
+
+
+# Collect answers
+answers = {
+    # In total, how many pounds have these athletes Bench Pressed?
+    'q1': q1(),
+
+    # How many pounds did Barry Moore Back Squat in 2016?
+    'q2': q2(),
+
+    # In what month of 2017 did Barry Moore Back Squat the most total weight?
+    'q3': None,
+
+    # What is Abby Smith's Bench Press PR weight?
+    # PR defined as the most the person has ever lifted for that exercise, regardless of reps performed.
+    'q4': None
+}
+
+print(json.dumps(answers))
